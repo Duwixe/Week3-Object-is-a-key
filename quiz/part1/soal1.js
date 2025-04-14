4feee8c6e2300b20dc6514a5bf2e0ebe2a6a7b27
Duwixe <duwixefalufi@gmail.com>
2025-04-14 14:25:00 +0700

    Belajar

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,12 @@
+//cek apakah angka yang dikirim adalah angka prima atau bukan?
+//cek google bagi yang ga tau apa itu angka prima
+function angkaPrima(angka) {
+    let angka = "";
+  }
+  
+  // TEST CASES
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
